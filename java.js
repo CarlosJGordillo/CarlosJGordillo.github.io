164b033d3fc21c5ddff4558d4b5a68defd4ca0e9
CarlosJGordillo <123968058+CarlosJGordillo@users.noreply.github.com>
2023-01-30 12:24:22 -0300

    Delete java.js

--- java.js
+++
@@ -1,8 +0,0 @@
-function cambiarColor() {
-    document.body.style.backgroundColor = "#151E21" ;
-    document.body.style.color = "white";
-    document.getElementById("nom").style.color="red" ; 
-    document.getElementById("barra1").style.backgroundColor ="#4e4e4e";
-    document.getElementById("barra2").style.backgroundColor ="#4e4e4e";
-    document.getElementById("titulo").style.color ="#27c5a9";
-}
